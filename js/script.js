66036d89867c7e1c5abc361254b882a51eae6bf6
psyabrprog <psyabrprog@mail.ru>
2022-05-26 21:58:11 +0300

    first create app

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 "use strict";
 
-alert("Gotovo");
+const numberOfFilms = +prompt("Cколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Один из последних просмотренных фильмов", ""),
+  b = prompt("На сколько его оцените?", ""),
+  c = prompt("Один из последних просмотренных фильмов", ""),
+  d = prompt("На сколько его оцените?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
